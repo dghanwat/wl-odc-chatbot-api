8df2ab2dcce6b1a343309e395124b24835416db4
Dhananjay Ghanwat <dghanwat>
2018-06-22 21:29:21 +0530

    intial implementation of chatterbot

--- api/api.py
+++ api/api.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
 api = Api(app)
+chatBot = ChatBot("WL ODC")
 
 
 class HelloWorld(Resource):
@@ -10,7 +14,41 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 
+class ChatBotTrain(Resource):
+    def post(self):
+        conversation = [
+            "Hello",
+            "Hi there!",
+            "How are you doing?",
+            "I'm doing great.",
+            "That is good to hear",
+            "Thank you.",
+            "You're welcome.",
+            "What is WL ODC",
+            "WL ODC is a great place to work"
+        ]
+        chatBot.set_trainer(ChatterBotCorpusTrainer)
+        chatBot.set_trainer(ListTrainer)
+        chatBot.train(
+            "chatterbot.corpus.english"
+        )
+        chatBot.train(conversation)
+        return {'response': 'Agent training completed successfully'}
+
+
+class ChatBot(Resource):
+
+    def get(self):
+        response = str(chatBot.get_response("What is WL ODC"))
+
+        print("Response from Bot is ", str(response))
+        return {'response': response}
+
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(ChatBotTrain, '/chat/train')
+api.add_resource(ChatBot,
+                 '/chat')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
